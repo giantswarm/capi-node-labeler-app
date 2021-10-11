be17d930a9638f3f9837aafbcf85f8560c11f557
calvix <vaclav@giantswarm.io>
2021-10-11 12:49:50 +0200

    init

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/giantswarm/capi-node-labeler-app
+
+go 1.16
+
+require (
+	k8s.io/api v0.22.2
+	k8s.io/apimachinery v0.22.2
+	k8s.io/client-go v0.22.2
+	k8s.io/utils v0.0.0-20210819203725-bdf08cb9a70a
+	sigs.k8s.io/controller-runtime v0.10.2
+)
---
+++ main.go
@@ -0,0 +1,123 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+	"k8s.io/utils/env"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	//+kubebuilder:scaffold:imports
+)
+
+const (
+	MasterNodeRoleLabel   = "node-role.kubernetes.io/master"
+	LegacyMasterNodeLabel = "node.kubernetes.io/master"
+
+	WorkerNodeRoleLabel   = "node-role.kubernetes.io/worker"
+	LegacyWorkerNodeLabel = "node.kubernetes.io/worker"
+)
+
+var (
+	scheme = runtime.NewScheme()
+)
+
+func init() {
+	_ = clientgoscheme.AddToScheme(scheme)
+
+	//+kubebuilder:scaffold:scheme
+}
+
+func main() {
+	nodeName := env.GetString("NODE_NAME", "")
+	if nodeName == "" {
+		fmt.Printf("ERROR: NODE_NAME env cannot be empty")
+		os.Exit(1)
+	}
+
+	config, err := ctrl.GetConfig()
+	if err != nil {
+		fmt.Printf("ERROR: failed to get config for controlelr runtime client")
+		panic(err)
+	}
+
+	ctrlClient, err := client.New(config, client.Options{})
+	if err != nil {
+		fmt.Printf("ERROR: failed to create controller runtime client")
+		panic(err)
+	}
+
+	ctx := context.TODO()
+	var node v1.Node
+
+	err = ctrlClient.Get(ctx, client.ObjectKey{Name: nodeName}, &node)
+	if err != nil {
+		fmt.Printf("ERROR: failed to get node %s", nodeName)
+		panic(err)
+	}
+
+	shouldUpdate := false
+	// check if the node is worker or master
+	// master will already have the label set, worker wont
+	if hasLabel(node.Labels, MasterNodeRoleLabel) {
+		// master node
+		if !hasLabel(node.Labels, LegacyMasterNodeLabel) {
+			node.Labels[LegacyMasterNodeLabel] = ""
+			shouldUpdate = true
+		}
+
+	} else {
+		// worker node
+		if !hasLabel(node.Labels, WorkerNodeRoleLabel) {
+			node.Labels[WorkerNodeRoleLabel] = ""
+			shouldUpdate = true
+		}
+		if !hasLabel(node.Labels, LegacyWorkerNodeLabel) {
+			node.Labels[LegacyWorkerNodeLabel] = ""
+			shouldUpdate = true
+		}
+	}
+
+	if shouldUpdate {
+		err = ctrlClient.Update(ctx, &node)
+		if err != nil {
+			fmt.Printf("ERROR: failed to apply new labels")
+			panic(err)
+		}
+		fmt.Printf("new labels applied to node")
+	} else {
+		fmt.Printf("required labels are already applied")
+	}
+
+	fmt.Printf("capi-node-labeler finished successfully")
+}
+
+func hasLabel(labels map[string]string, labelName string) bool {
+	for k, _ := range labels {
+		if k == labelName {
+			return true
+		}
+	}
+	return false
+}
